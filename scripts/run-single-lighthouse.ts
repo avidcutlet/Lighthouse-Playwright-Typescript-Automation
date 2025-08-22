c1118c80fd51e2481e0b21794a522b7c0fe5bb19
VonWebsterLabajo <vwlabajo@cheqsystems.com>
2025-08-22 13:23:27 +0800

    refactor(files): fix run-single-lighthouse and excel template structure
    - Align config of run-single-lighthouse with run-all-lighthouse
    - Update excel template to fix #CALC! error

--- scripts/run-single-lighthouse.ts
+++ scripts/run-single-lighthouse.ts
@@ -1,43 +1,50 @@
 import path from 'path';
 
-import { SINGLE_TEST_URL } from '@config/lighthouse.config';
-import { folderTimestamp } from '@config/lighthouse.config';
+import { SINGLE_TEST_URL, OUTPUT_FOLDER_TIMESTAMP } from '@config/lighthouse.config';
+import { screenshotOption } from '@config/lighthouse.config';
 
 import { runLighthouse } from '@utils/lighthouse-runner-util';
-import { askScreenshotOption } from '@utils/user-input-util';
 import { prepareExcelCopy, writeAllToExcel } from '@utils/excel-writer-util';
-import { getLighthouseOutputPaths } from '@utils/report-path-util';
+import { arrangeFiles, getLighthouseOutputPaths } from '@utils/report-path-util';
+import { startEmojiSpinner, clearLine, stopEmojiSpinner } from '@utils/spinner-util';
 
 const devices: ('Mobile' | 'Desktop')[] = ['Mobile', 'Desktop'];
 const modes: boolean[] = [false, true]; // false = normal, true = incognito
 
 const lighthouseRuns: Promise<void>[] = [];
 
+let currentIndex: number = 0;
+
 // Run all 4 in parallel and wait for all to finish
 (async () => {
-  const screenshotOption = await askScreenshotOption();
+    // IN PROGRESS...
+  // const screenshotOption = await askScreenshotOption();
 
   const url = [SINGLE_TEST_URL];
   let taskIndex = 0;
   const totalTasks = url.length * devices.length * modes.length;
 
-  const outputDir = await getLighthouseOutputPaths(folderTimestamp);
+  const outputDir = await getLighthouseOutputPaths(`lighthouse-${OUTPUT_FOLDER_TIMESTAMP}`);
   const excelPath = prepareExcelCopy(outputDir);
+
+    // Overall run time tracker
+  const startTime = Date.now();
+  console.log(`🕐 Started at: ${new Date(startTime).toLocaleString()}\n`);
+
+  startEmojiSpinner(`Lighthouse is running`);
   
   for (const device of devices) {
     for (const isIncognito of modes) {
       const modeLabel = isIncognito ? 'Incognito' : 'Normal';
       const label = `${device}-${modeLabel}`;
 
-      let currentIndex = taskIndex++;
+      currentIndex = taskIndex++;
 
       lighthouseRuns.push(runLighthouse(
         url.toString(),
         device,
         isIncognito,
         screenshotOption,
-        currentIndex,
-        totalTasks,
         label,
         outputDir,
       ));
@@ -46,9 +53,27 @@ const lighthouseRuns: Promise<void>[] = [];
 
   await Promise.all(lighthouseRuns);
 
+  // Arrange after all report generation
+  if (currentIndex === totalTasks - 1) {
+    console.log('\n🧹 Arranging files on last run...');
+    await arrangeFiles(outputDir);
+    const trimmedReportDir = path.relative(__dirname, outputDir);
+    console.log(`\n✅ Lighthouse report saved in: ${trimmedReportDir}`);
+  }
+
   await writeAllToExcel(
     path.join(outputDir, 'lighthouse-simplified-data.txt'),
     excelPath
   );
+
+  stopEmojiSpinner();
   console.log('\n🌟 All Lighthouse runs completed.');
+  
+  // Overall run time tracker
+  const endTime = Date.now();
+  const durationSec = (endTime - startTime) / 1000;
+  const minutes = Math.floor(durationSec / 60);
+  const seconds = (durationSec % 60).toFixed(2);
+
+  console.log(`🕐 Duration: ${minutes}m ${seconds}s`);
 })();
